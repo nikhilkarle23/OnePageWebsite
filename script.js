66676d39f5f884119b65d8d7dc93588c9f0fcdc1
nikhilkarle23 <nikhilkarlework@gmail.com>
2023-08-31 14:22:21 +0530

    OnePageWebsite Template

---
+++ script.js
@@ -0,0 +1,9 @@
+const signUpButton = document.getElementById('customeSignUp');
+const signInButton = document.getElementById('customSignIn');
+const container = document.getElementById('custom-container');
+
+signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
+signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
+
+
+// container.classList.add('right-panel-active'));
