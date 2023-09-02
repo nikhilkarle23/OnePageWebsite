a542e45dde2be1a8f9f538c73c75735b74f548a2
nikhilkarle23 <nikhilkarlework@gmail.com>
2023-09-02 20:29:09 +0530

    Login and Register removed

--- script.js
+++
@@ -1,6 +0,0 @@
-const signUpButton = document.getElementById('customeSignUp');
-const signInButton = document.getElementById('customSignIn');
-const container = document.getElementById('custom-container');
-
-signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
-signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
