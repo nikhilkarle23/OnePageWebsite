230e427e03903d067e19ce20618b25378ad2f674
nikhilkarle23 <nikhilkarlework@gmail.com>
2023-09-02 13:23:36 +0530

    Updated changes

--- script.js
+++ script.js
@@ -4,6 +4,3 @@ const container = document.getElementById('custom-container');
 
 signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
 signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
-
-
-// container.classList.add('right-panel-active'));
